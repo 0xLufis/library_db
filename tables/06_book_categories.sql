b07d8a0c05234ce8f6effd23f24b16bf9e98a1ab
0xLufis <filus.bendeguz@gmail.com>
2025-12-05 22:20:46 +0100

    Added more tables

--- tables/06_book_categories.sql
+++ tables/06_book_categories.sql
@@ -2,9 +2,7 @@ create table book_categories (
        book_id number(10) not null,
        category_id number(10) not null,
        
-       
        constraint fk_bc_book foreign key (book_id) references books (book_id) ON DELETE CASCADE, --Ha nincs konyv nincs kategoria
        constraint fk_bc_category foreign key (category_id) references categories (category_id),
        constraint pk_book_categories primary key (book_id, category_id)
-       
-)
+);
---
+++ tables/07_loans.sql
@@ -0,0 +1,20 @@
+create sequence seq_loan_id 
+start with 1
+increment by 1
+minvalue 100;
+
+create table loans (
+       loan_id number(10) not null,
+       customer_id number(10) not null,
+       loan_date date default sysdate,
+       return_by_date date not null,
+       employee_id number(10) not null,
+       returned_at_date date,
+       fine_amount number(8,2) default 0,
+       
+     
+       constraint pk_loan primary key (loan_id),
+       constraint fk_loan_customer_id foreign key (customer_id) references cusotmers (customer_id),
+       constraint foreign fk_loan_employee_id key (employee_id) references employees (employee_id),
+       
+);
