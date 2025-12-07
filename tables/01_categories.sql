102aa28ecd5defb6ddbabd9e83ed598e6cf49cce
0xLufis <filus.bendeguz@gmail.com>
2025-12-07 23:11:01 +0100

    Removed unused and defunct files

--- tables/01_categories.sql
+++
@@ -1,8 +0,0 @@
-CREATE SEQUENCE seq_categories_id START WITH 1 INCREMENT BY 1;
-create table categories (
-       category_id number(5) NOT NULL,
-       category_name VARCHAR2(50) NOT NULL, --50 karakterbe csak belefer minden
-       constraint pk_categories primary key (category_id),
-       constraint uk_category_name unique (category_name)
-) tablespace users;
-/
